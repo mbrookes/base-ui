cda2fd6b28882d5445a2aeff573ed36ef126eb67
Matt <github@nospam.33m.co>
2026-04-11 18:21:11 +0100

    [file-upload] Restore prop filtering for customized renders
    Keep non-DOM props out of element prop forwarding for HiddenInput and
    Trigger by excluding render/className before passing props to
    useRenderElement.
    
    This fixes conformance warnings like:
    - Invalid value for prop 'render' on <div> tag
    
    Validation:
    - FileUploadConformance tests pass (45/45)
    - FileUploadTrigger tests pass (15/15)

--- packages/react/src/file-upload/input/FileUploadInput.tsx
+++ packages/react/src/file-upload/input/FileUploadInput.tsx
@@ -28,6 +28,7 @@ export const FileUploadHiddenInput = React.forwardRef(function FileUploadHiddenI
   componentProps: FileUploadHiddenInputProps,
   forwardedRef: React.ForwardedRef<HTMLInputElement>,
 ) {
+  const { render, className, ...elementProps } = componentProps;
   const { inputId, accept, multiple, directory, disabled, setInputElement, onInputChange } =
     useFileUploadContext();
 
@@ -56,7 +57,7 @@ export const FileUploadHiddenInput = React.forwardRef(function FileUploadHiddenI
         style: { display: 'none' },
         onChange: onInputChange,
       },
-      componentProps,
+      elementProps,
     ],
   });
 });
--- packages/react/src/file-upload/trigger/FileUploadTrigger.tsx
+++ packages/react/src/file-upload/trigger/FileUploadTrigger.tsx
@@ -43,7 +43,7 @@ export const FileUploadTrigger = React.forwardRef(function FileUploadTriggerComp
   componentProps: FileUploadTriggerProps,
   ref: React.ForwardedRef<HTMLElement>,
 ) {
-  const { nativeButton = true, ...elementProps } = componentProps;
+  const { render, className, nativeButton = true, ...elementProps } = componentProps;
   const { openFileDialog, disabled } = useFileUploadContext();
 
   const { getButtonProps, buttonRef } = useButton({
